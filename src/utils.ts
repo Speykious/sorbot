af428f03c4c3f17c120fb5ce0564936ec2226ef5
Speykious <speykious.the.king@gmail.com>
2020-06-24 00:01:23 +0200

    Ok, right... We'll use spaces -__-

--- src/utils.ts
+++ src/utils.ts
@@ -3,16 +3,16 @@ import * as path from "path"
 
 export const relative = (s: string): string => path.resolve(__dirname, s)
 export const delay = async (ms: number): Promise<void> =>
-	new Promise((resolve) => setTimeout(resolve, ms))
+  new Promise((resolve) => setTimeout(resolve, ms))
 
 export const sendError = async (
-	channel: TextChannel | DMChannel | NewsChannel,
-	errorString: string,
-	msDelay = 5000
+  channel: TextChannel | DMChannel | NewsChannel,
+  errorString: string,
+  msDelay = 5000
 ): Promise<void | Message> => {
-	const errorMsg: void | Message = await channel
-		.send(errorString)
-		.catch(() => Promise.resolve())
-	if (errorMsg) errorMsg.delete({ timeout: msDelay })
-	return Promise.resolve(errorMsg)
+  const errorMsg: void | Message = await channel
+    .send(errorString)
+    .catch(() => Promise.resolve())
+  if (errorMsg) errorMsg.delete({ timeout: msDelay })
+  return Promise.resolve(errorMsg)
 }
