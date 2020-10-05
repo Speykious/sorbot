f429e568ead417e0f9c8781a518661cdf8cabb5a
Speykious <speykious@gmail.com>
2020-10-05 23:29:00 +0200

    The bundle will now be inside the local repo (and ignored of course)
    Signed-off-by: Speykious <speykious@gmail.com>

--- bundle.sh
+++ bundle.sh
@@ -7,21 +7,21 @@ u='\e[4m'
 c='\e[0m'
 
 echo -e "  ${bred}Clearing$c    the ${u}sorbot-min/$c folder"
-rm -r ../sorbot-min/
-mkdir ../sorbot-min/
+rm -r sorbot-min/
+mkdir sorbot-min/
 
 echo -e "  ${bred}Compiling$c   the coffeescript code into javascript code"
 coffee -bcmo client src
 
 echo -e "  ${bred}Minifying$c   the javascript code"
-ncc build client/index.js -mo ../sorbot-min/bin/
+ncc build client/index.js -mo sorbot-min/bin/
 
 echo -e "  ${bred}Copying$c     the needed resources to ${u}sorbot-min/$c"
-rsync -rah --progress resources ../sorbot-min/
-rsync -ah --progress credentials.yaml token.yaml ../sorbot-min/
-rsync -ah --progress bundle.env ../sorbot-min/.env
+rsync -rah --progress resources sorbot-min/
+rsync -ah --progress credentials.yaml token.yaml sorbot-min/
+rsync -ah --progress bundle.env sorbot-min/.env
 
 echo -e "  ${bred}Removing$c    the ${u}bin/lib/$c folder"
-rm -rf ../sorbot-min/bin/lib/
+rm -rf sorbot-min/bin/lib/
 
 echo -e "  ${bgrn}Success.$c"
